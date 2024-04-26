46a91f5bcd0ef734b5b9c3bd723d539232f91235
Allan182 <allan.furlani@hotmail.com>
2024-04-26 19:33:47 -0300

    Add Decorator Personalizado @EmailUnico

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -6,7 +6,7 @@ export class criaUsuarioDTO {
     @IsString({ message: "Insira Caracteres Validos" })
     @IsNotEmpty({ message: "O Nome não pode ser vazio!" })
     nome: String;
-
+ 
     @EmailUnico({ message: "Já Existe um Usuário com este E-mail!"})
     @IsEmail(undefined, { message: "Insira um E-mail Válido!" })
     email: String;
