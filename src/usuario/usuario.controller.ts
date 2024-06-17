de17b34282ee49118a011cd3c1beea206f9681d1
Allan182 <allan.furlani@hotmail.com>
2024-06-17 15:59:51 -0300

    feat: Create UsuarioEntity

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,16 +1,27 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { criaUsuarioDTO } from "./dto/CriaUsuario.dto";
+import { UsuarioEntity } from "./usuario.entity";
+import { v4 as uuid } from 'uuid';
 
 @Controller('/usuarios')
 export class UsuarioController {
- 
+
     constructor(private usuarioRepository: UsuarioRepository) { }
 
     @Post()
-    async criaUsuario(@Body() dadosDoUsuario: criaUsuarioDTO) {    
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario;
+    async criaUsuario(@Body() dadosDoUsuario: criaUsuarioDTO) {
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+        return {
+            id: usuarioEntity.id,
+            message: 'Usuário Criado com Sucesso!'
+        };
     }
 
     @Get()
